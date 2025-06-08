3f7a9bb6c71ce7c0da0fbbb91044b24d74fc9ba2
psns <psns0122@naver.com>
2025-06-09 06:53:00 +0900

    feat: 상품 상세 조회 성공

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -35,6 +35,11 @@ const router = createRouter({
       component: () => import('@/views/lawyer/template/TemplateFormView.vue')
     },
     {
+      path: '/lawyer/templates/:no',
+      name: 'TemplateDetail',
+      component: () => import('@/views/lawyer/template/TemplateDetailView.vue')
+    },
+    {
       path: '/lawyer/templates/edit/:no',
       name: 'TemplateEdit',
       component: () => import('@/views/lawyer/template/TemplateFormView.vue'),
