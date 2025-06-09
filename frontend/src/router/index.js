72c196915f81d019f26113a6184899b5f954150d
psns <psns0122@naver.com>
2025-06-09 16:56:04 +0900

    refactor: index.js 권한별 분할

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,128 +1,15 @@
-// src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
+import commonRoutes from './common'
+import lawyerRoutes from './lawyer'
+import clientRoutes from './client'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: () => import('@/views/HomeView.vue')
-        },
-        {
-            path: '/login',
-            name: 'Login',
-            component: () => import('@/views/common/LoginView.vue')
-        },
-        {
-            path: '/lawyer',
-            name: 'lawyer',
-            component: () => import('@/views/lawyer/DashboardView.vue')
-        },
-        {
-            path: '/lawyer/reservation',
-            name: 'LawyerReservation',
-            component: () => import('@/views/lawyer/ReservationView.vue')
-        },
-        {
-            path: '/lawyer/broadcasts',
-            name: 'LawyerBroadcasts',
-            component: () => import('@/views/lawyer/BroadcastScheduleView.vue')
-        },
-        {
-            path: '/lawyer/templates',
-            name: 'LawyerTemplateList',
-            component: () => import('@/views/lawyer/template/TemplateListView.vue')
-        },
-        {
-            path: '/lawyer/templates/register',
-            name: 'TemplateRegister',
-            component: () => import('@/views/lawyer/template/TemplateFormView.vue')
-        },
-        {
-            path: '/lawyer/templates/:no',
-            name: 'LawyerTemplateDetail',
-            component: () => import('@/views/lawyer/template/TemplateDetailView.vue')
-        },
-        {
-            path: '/lawyer/templates/edit/:no',
-            name: 'TemplateEdit',
-            component: () => import('@/views/lawyer/template/TemplateFormView.vue'),
-            props: true
-        },
-        {
-            path: '/lawyer/ads',
-            name: 'LawyerAds',
-            component: () => import('@/views/lawyer/AdManagementView.vue')
-        },
-        {
-            path: '/lawyer/qna',
-            name: 'LawyerQna',
-            component: () => import('@/views/lawyer/QnaListView.vue')
-        },
-        {
-            path: '/lawyer/profile',
-            name: 'LawyerProfile',
-            component: () => import('@/views/lawyer/ProfileSettingView.vue')
-        },
-        {
-            path: '/lawyer/signup',
-            name: 'LawyerSignup',
-            component: () => import('@/views/common/LawyerSignupView.vue')
-        },
-        {
-            path: '/client/reservations/:lawyerNo/:lawyerName',
-            name: 'ClientReservations',
-            component: () => import('@/views/client/ClientReservationsView.vue')
-        },
-        {
-            path: '/client/signup',
-            name: 'ClientSignup',
-            component: () => import('@/views/common/ClientSignupView.vue')
-        },
-        {
-            path: '/client/mypage',
-            name: 'ClientMypage',
-            component: () => import('@/views/client/MypageView.vue')
-        },
-        {
-            path: '/client/templates',
-            name: 'ClientTemplates',
-            component: () => import('@/views/client/TemplateView.vue')
-        },
-        {
-            path: '/broadcasts',
-            name: 'ClientBroadcasts',
-            component: () => import('@/views/client/ClientBroadcasts.vue')
-        },
-        {
-            path: '/templates',
-            name: 'ClientTemplateList',
-            component: () => import('@/views/client/template/TemplateListView.vue')
-        },
-        {
-            path: '/templates/:no',
-            name: 'ClientTemplateDetail',
-            component: () => import('@/views/client/template/TemplateDetailView.vue')
-        },
-        {
-            path: '/qna',
-            name: 'ClientQaList',
-            component: () => import('@/views/common/QaListView.vue')
-        },
-        // 로그인한 “client” 권한으로만 글쓰기
-        {
-            path: '/client/qna/register',
-            name: 'ClientQaRegister',
-            component: () => import('@/views/client/ClientQaRegister.vue'),
-            meta: { requiresAuth: true, role: 'client' }
-        },
-        {
-            path: '/qna/:no',
-            name: 'ClientQaDetail',
-            component: () => import('@/views/common/QaDetailView.vue'),
-        },
+        ...commonRoutes,
+        ...lawyerRoutes,
+        ...clientRoutes,
     ]
 })
 
-export default router
+export default router;
