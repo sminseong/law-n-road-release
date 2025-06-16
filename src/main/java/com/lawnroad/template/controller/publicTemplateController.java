98509f2b5e48cd8184b69c5dba0016c05cf7ea39
psns <psns0122@naver.com>
2025-06-16 16:29:26 +0900

    feat: public 권한 추가

---
+++ src/main/java/com/lawnroad/template/controller/publicTemplateController.java
@@ -0,0 +1,11 @@
+package com.lawnroad.template.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/public/templates")
+public class publicTemplateController {
+}
