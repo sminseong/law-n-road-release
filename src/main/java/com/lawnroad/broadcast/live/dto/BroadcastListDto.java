940263efce8d1a8fe9777ae7c417ca64f87978d4
sminseong <star919m@gmail.com>
2025-06-18 14:55:25 +0900

    feat: 방송 리스트 백엔드 및 프론트 구현 완료

--- src/main/java/com/lawnroad/broadcast/live/dto/BroadcastListDto.java
+++ src/main/java/com/lawnroad/broadcast/live/dto/BroadcastListDto.java
@@ -2,11 +2,13 @@ package com.lawnroad.broadcast.live.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 public class BroadcastListDto {
     private Long broadcastNo;       // 방송 고유 번호
+    private LocalDateTime startTime;// 시작 시간
     private String title;           // 방송 제목
     private String thumbnailPath;   // 썸네일 경로
 
@@ -15,5 +17,5 @@ public class BroadcastListDto {
     private List<String> keywords;  // 키워드 목록
 
     private String lawyerName;      // 변호사 이름
-    private String profilePath;     // 변호사 프로필
+    private String profile;     // 변호사 프로필
 }
