dce57012db1c702a29424958ed7b2b077869341d
psns <psns0122@naver.com>
2025-07-01 14:19:06 +0900

    feat: 매달 1일 포인트 초기화

--- src/main/java/com/lawnroad/account/mapper/LawyerPointMapper.java
+++ src/main/java/com/lawnroad/account/mapper/LawyerPointMapper.java
@@ -3,10 +3,15 @@ package com.lawnroad.account.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface LawyerPointMapper {
   
   // 포인트 증감 (음수도 허용)
   int updatePoint(@Param("lawyerNo") Long lawyerNo, @Param("amount") int amount);
+  
+  // 매달 1일에 모든 변호사의 point를 0으로
+  @Update("UPDATE lawyer SET point = 0")
+  int resetAllPoints();
 }
