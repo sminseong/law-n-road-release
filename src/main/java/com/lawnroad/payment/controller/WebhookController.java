a049e3d41cc419ad4ce95a73fde67ae4c61f0b6b
KangChangSeon <rlckdtjs@naver.com>
2025-07-01 12:46:23 +0900

    feat: 웹훅 기능 수정

--- src/main/java/com/lawnroad/payment/controller/WebhookController.java
+++ src/main/java/com/lawnroad/payment/controller/WebhookController.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lawnroad.payment.service.WebhookService;
-import com.lawnroad.payment.verifier.TossWebhookVerifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,29 +13,20 @@ import org.springframework.web.bind.annotation.*;
 public class WebhookController {
 
     private final WebhookService webhookService;
-    private final TossWebhookVerifier verifier;
     private final ObjectMapper objectMapper;
 
     public WebhookController(
             WebhookService webhookService,
-            TossWebhookVerifier verifier,
             ObjectMapper objectMapper
     ) {
         this.webhookService = webhookService;
-        this.verifier = verifier;
         this.objectMapper = objectMapper;
     }
 
     @PostMapping
     public ResponseEntity<String> receiveWebhook(
-            @RequestHeader("Toss-Signature") String signature,
             @RequestBody String payload
     ) {
-        if (!verifier.isValid(payload, signature)) {
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body("Invalid signature");
-        }
 
         JsonNode json;
         try {
